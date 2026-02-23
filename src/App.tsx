5b43ddba59df3cf31eaf9216131b99883018ca26
hibiki <takechi.hibiki1234@gmail.com>
2026-02-23 21:40:24 +0900

    staff app

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,128 @@
 import { useState } from "react";
 import { v4 as uuid } from "uuid";
-type Product = {
+
+type Staff = {
   id:string;
   name:string;
-  price:number;
-  stock:number;
+  age:number;
+  department:Department;
 }
-type ProductItemProps = 
-Product
-&
-{addCart:(productId:string)=>void}
-function ProductItem({id, name, price, stock, addCart}:ProductItemProps){
+type Department = "IT"|"人事"|"営業";
+type DisplayDepartment = Department|"すべて";
+type DisplayStaffTableProps = {
+  staffs:Staff[];
+}
+const departmentSelect:Department[] = ["IT", "人事", "営業"]
+
+
+const displayDepartmentSelect:DisplayDepartment[] = ["すべて", ...departmentSelect]; 
+function DisplayStaffTable({staffs}:DisplayStaffTableProps){
+  const [selectedDepartment, setSelectedDepartment] = useState<DisplayDepartment>("すべて");
+  function handleInputChange(event:React.ChangeEvent<HTMLSelectElement, HTMLSelectElement>){
+    setSelectedDepartment(event.target.value as DisplayDepartment);
+  }
+  const filteredStaffs = selectedDepartment === "すべて" ? staffs : staffs.filter(staff => staff.department === selectedDepartment);
   return(
     <div>
-      <p>{name}</p>
-      <p>{price}</p>
-      <p>{stock}</p>
-      <div>
-        <button onClick={()=>addCart(id)} type="button" disabled={stock <= 0}>追加</button>
-      </div>
+      <DepartmentSelectList departmentSelect={displayDepartmentSelect} value={selectedDepartment} handleInputChange={handleInputChange}/>
+      <table>
+        <thead>
+          <tr>
+            <th>社員ID</th><th>名前</th><th>年齢</th><th>部署</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredStaffs.map(staff => {
+            return(
+              <tr key={staff.id}>
+                <td>{staff.id}</td><td>{staff.name}</td><td>{staff.age}</td><td>{staff.department}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
 
-const products:Product[] = [
-  {id:uuid(), name:"sample1", price:1000, stock:10},
-  {id:uuid(), name:"sample2", price:2000, stock:3},
-  {id:uuid(), name:"sample3", price:400, stock:13},
-];
-export default function ProductApp(){
-  const [currentProducts, setCurrentProducts] = useState<Product[]>(products);
-  const [totalPrice, setTotalPrice] = useState<number>(0);
-  function addCart(productId:string){
-    const newProducts = currentProducts.map(product => {
-      if(product.id === productId){
-        if(product.stock <= 0){
-          return product;
-        }
-        setTotalPrice(totalPrice + product.price);
-        return {...product, stock:product.stock - 1};
-      }
-      return product;
-    });
-    setCurrentProducts(newProducts);
+type RegisterStaffProps = {
+  registerStaff:Staff,
+  handleRegister:(event:React.SubmitEvent)=>void;
+  handleInputChange:<T extends HTMLInputElement|HTMLSelectElement>(event:React.ChangeEvent<T, T>)=>void;
+}
+function RegisterStaff({registerStaff, handleRegister, handleInputChange}:RegisterStaffProps){
+  return(
+    <form onSubmit={(event)=>handleRegister(event)}>
+      <div>
+        <label htmlFor="staff-name">名前: </label>
+        <input name="name" type="text" id="staff-name" value={registerStaff.name} onChange={(event)=>handleInputChange(event)}/>
+      </div>
+      <div>
+        <label htmlFor="staff-age">年齢: </label>
+        <input name="age" type="number" id="staff-age" value={registerStaff.age} onChange={(event)=>handleInputChange(event)}/>
+      </div>
+      <DepartmentSelectList departmentSelect={departmentSelect} value={registerStaff.department} handleInputChange={handleInputChange<HTMLSelectElement>}/>
+      <div>
+        <button type="submit">追加</button>
+      </div>
+    </form>
+  );
+}
+type DepartmentSelectListProps<U> = {
+  departmentSelect:U[],
+  value:U,
+  handleInputChange:<T extends HTMLSelectElement>(event:React.ChangeEvent<T, T>)=>void
+}
+function DepartmentSelectList<U extends Department|DisplayDepartment>({departmentSelect, value, handleInputChange}:DepartmentSelectListProps<U>){
+  return(
+    <div>
+      <label htmlFor="staff-department">
+          部署名: 
+          <select name="department" value={value} onChange={(event)=>handleInputChange(event)}>
+            {departmentSelect.map(department => <option key={department} value={department}>{department}</option>)}
+          </select>
+        </label>
+    </div>
+  );
+}
+const initialStaffs:Staff[] = [
+  {id:uuid(), name:"sample1", age:22, department:"IT"},
+  {id:uuid(), name:"sample2", age:30, department:"IT"}
+]
+const initialRegisterStaff:Staff = {id:"", name:"", age:25, department:"IT"}
+export default function StaffApp(){
+  const [currentStaffs, setCurrentStaffs] = useState<Staff[]>(initialStaffs);
+  const [registerStaff, setRegisterStaff] = useState<Staff>(initialRegisterStaff);
+  function handleRegister(event:React.SubmitEvent){
+    event.preventDefault();
+    if(registerStaff.name.trim() === ""){
+      return;
+    }
+    setCurrentStaffs([...currentStaffs, {...registerStaff, id:uuid(), name:registerStaff.name.trim()}]);
+    setRegisterStaff(initialRegisterStaff)
+  }
+  function handleInputChange<T extends HTMLInputElement|HTMLSelectElement>(event:React.ChangeEvent<T,T>){
+    const {name, value} = event.target;
+    if(name === "name"){
+      const newStaff:Staff = {...registerStaff, name:value};
+      setRegisterStaff(newStaff);
+      return;
+    }
+    if(name === "age"){
+      const newStaff:Staff = {...registerStaff, age:Number(value)};
+      setRegisterStaff(newStaff);
+      return;
+    }
+    if(name === "department"){
+      const newStaff:Staff = {...registerStaff, department:value as Department};
+      setRegisterStaff(newStaff);
+      return;
+    }
   }
   return(
     <>
-      <p>合計金額: {totalPrice}円</p>
-      {currentProducts.map(product => <ProductItem key={product.id} id={product.id} name={product.name} price={product.price} stock={product.stock} addCart={addCart}/>)}
+      <RegisterStaff registerStaff={registerStaff} handleRegister={handleRegister} handleInputChange={handleInputChange}/>
+      <DisplayStaffTable staffs={currentStaffs}/>
     </>
   );
 }
